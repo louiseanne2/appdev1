828de7e5542426ef63a56741e9a4b05bf2d7febc
Louise <louiseannesuriobechayda@gmail.com>
2025-09-03 22:22:10 +0800

    Add variables activity

---
+++ 11_javascript-refresher/02_variables.js
@@ -0,0 +1,23 @@
+const name = "Louise";
+const age = 20;
+const isStudent = true;
+
+console.log("Name:", name)        
+console.log("Age:", age)          
+console.log("Is Student:", isStudent) 
+
+
+console.log(typeof name)  
+console.log(typeof age)   
+console.log(typeof isStudent) 
+
+let x = 10; 
+let y = 5;
+
+console.log("Addition:", x + y)      
+console.log("Subtraction:", x - y)  
+console.log("Multiplication:", x * y)
+console.log("Division:", x / y) 
+
+console.log(5 == "5")   
+console.log(5 === "5")  
