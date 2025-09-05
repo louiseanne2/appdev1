934c49fa8a0adf580ed93f05813b39869ac4b11e
Louise <louiseannesuriobechayda@gmail.com>
2025-09-05 19:45:57 +0800

    Add let and const activity

---
+++ 12_javascript-refresher-continuation/10_let_const.js
@@ -0,0 +1,17 @@
+var name = "Louise";
+var age = 20;
+age = 19;
+
+console.log("Var: ", name, age);
+
+let myName = "Louise";  
+let myAge = 20;
+myAge = 19;
+
+console.log("Let: ", myName, myAge);
+
+const userName = "Louise";
+const userAge = 20;
+userAge = 19;
+
+console.log("Const: ", userName, userAge);
