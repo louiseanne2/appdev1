e8270d9ec1eef781440820a384d41b1b7e2150bb
Louise <louiseannesuriobechayda@gmail.com>
2025-09-05 19:47:03 +0800

    Add arrow functions activity

---
+++ 12_javascript-refresher-continuation/11_arrow_functions.js
@@ -0,0 +1,14 @@
+//one parameter
+const greet = (name) => {
+    console.log("Hello, " + name);
+}
+
+
+//implicit return
+const square = n => n * n;
+
+
+//no parameters
+const sayHi = () => {
+    console.log("Hello!");
+}
