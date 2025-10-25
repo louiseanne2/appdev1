24eece14b7660ace12e3cadebd6e02db3cd514a7
Louise <louiseannesuriobechayda@gmail.com>
2025-10-25 14:55:24 +0800

    Done with how to conditionally render components

--- react-describing-ui/src/App.jsx
+++ react-describing-ui/src/App.jsx
@@ -1,35 +1,29 @@
-import { getImageUrl } from './utils.jsx'
-
-export default function Profile() {
+function Item({ name, isPacked }) {
   return (
-    <Card>
-      <Avatar
-        size={100}
-        person={{
-          name: 'Katsuko Saruhashi',
-          imageId: 'YfeOqp2'
-        }}
-      />
-    </Card>
+    <li className="item">
+      {name} {isPacked && '✅'}
+    </li>
   );
 }
 
-function Avatar({ person, size }) {
+export default function PackingList() {
   return (
-    <img
-      className="avatar"
-      src={getImageUrl(person)}
-      alt={person.name}
-      width={size}
-      height={size}
-    />
-  );
-}
-
-function Card({ children }) {
-  return (
-    <div className="card">
-      {children}
-    </div>
+    <section>
+      <h1>Sally Ride's Packing List</h1>
+      <ul>
+        <Item
+          isPacked={true}
+          name="Space suit"
+        />
+        <Item
+          isPacked={true}
+          name="Helmet with a golden leaf"
+        />
+        <Item
+          isPacked={false}
+          name="Photo of Tam"
+        />
+      </ul>
+    </section>
   );
 }
