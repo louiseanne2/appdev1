ae84347386428b8a5c036cbb170e87c8212c366b
Louise <louiseannesuriobechayda@gmail.com>
2025-10-25 15:33:49 +0800

    Done with why understanding your UI as trees is useful

--- react-describing-ui/src/App.jsx
+++ react-describing-ui/src/App.jsx
@@ -1,31 +1,35 @@
-// let guest = 0;
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-// function Cup() {
-  
-//   guest = guest + 1;
-//   return <h2>Tea cup for guest #{guest}</h2>;
-// }
+function App() {
+  const [count, setCount] = useState(0)
 
-// export default function TeaSet() {
-//   return (
-//     <>
-//       <Cup />
-//       <Cup />
-//       <Cup />
-//     </>
-//   );
-// }
-
-function Cup({ guest }) {
-  return <h2>Tea cup for guest #{guest}</h2>;
-}
-
-export default function TeaSet() {
   return (
     <>
-      <Cup guest={1} />
-      <Cup guest={2} />
-      <Cup guest={3} />
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
-  );
+  )
 }
+
+export default App
